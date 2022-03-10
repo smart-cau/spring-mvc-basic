faa4745b31c37280139b2567da28cf4ba3f26494
HyeonWooJung <1995hyunwoo@gmail.com>
2022-03-10 22:16:17 +0900

    2. Servlet / JSP / MVC 패턴 > 회원관리 웹 app > MVC로 구현

---
+++ src/main/java/hello/servlet/web/servelt_mvc/MvcMemberSaveServlet.java
@@ -0,0 +1,36 @@
+package hello.servlet.web.servelt_mvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        // Model에 데이터를 보관! request 객체에 data를 보관해서 view에 전달 가능. view는 request.getAttreubute()로 꺼낼 수 있음
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/save-result.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+성공
+<ul>
+<%--  ${member.id} 자체가 <%= ((Member)request.getAttribute()).getId() %>의 뜻임.   --%>
+    <li>id=${member.id}</li>
+    <li>username=${member.username}</li>
+    <li>age=${member.age}</li>
+</ul>
+<a href="/index.html">메인</a>
+</body>
+</html>
